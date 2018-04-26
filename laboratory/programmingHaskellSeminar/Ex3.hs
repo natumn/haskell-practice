acc2d539b7a14b571ed99125fb37f8023297cde9
natumn <natsume.pcuse75@gmail.com>
2018-04-26 12:29:21 +0900

    seminar

---
+++ laboratory/programmingHaskellSeminar/Ex3.hs
@@ -0,0 +1,10 @@
+module Ex3 where
+
+second :: [a] -> a
+second xs = head (tail xs)
+
+swap :: (a, b) -> (b, a)
+swap (x, y) = (y, x)
+
+pair :: a -> b -> (a, b)
+pair x y = (x, y)
--- my-practice/caseof.hs
+++ my-practice/caseof.hs
@@ -21,6 +21,18 @@ qsort (n : xs) = qsort lt ++ [n] ++ qsort gteq
     lt   = [ x | x <- xs, x < n ]
     gteq = [ x | x <- xs, x >= n ]
 
+data Color = Blue | Red | Green | Yellow deriving (Show, Enum)
+data Point = Point Int Int deriving Show
+
+offset (Point x1 x2) (Point x3 x4) = Point (x1 + x2) (x3 + x4)
+
+data Rect = Rect Int Int Int Int
+          | Rect3D Int Int Int Int Int Int
+          deriving Show
+
+contains (Rect x y z w) (Point p1 p2) =
+    x <= p1 && p1 < x + w && y <= p2 && p2 < y + w
+
 main = do
     print $ fact 5
     print $ length' [1, 2, 3, 4]
@@ -28,12 +40,31 @@ main = do
     print $ product' [1, 2, 3, 4]
     print $ drop' 2 [1, 2, 3, 4]
     print $ qsort [4, 6, 9, 8, 3, 5, 1, 7, 2]
-    print $ [(x, y) | x <- [1..3], y <- "abc"]
+    print $ [ (x, y) | x <- [1 .. 3], y <- "abc" ]
 
 
     let p3 = (1, 2, 3)
     print p3
     print [ fact x | x <- [1 .. 4], x < 3 ]
+    print Blue
+    print $ fromEnum Green
+    print (toEnum 2 :: Color)
+    let a = Point 2 3
+        b = Point 1 1
+        c = offset a b
+    print c
+    print $ contains (Rect 2 2 3 3) (Point 2 2)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
